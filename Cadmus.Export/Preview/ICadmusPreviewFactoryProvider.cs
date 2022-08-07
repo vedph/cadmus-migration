69ccdb6cfca36c1a2d773f2c2298bc5e30c266e7
Myrmex <dfusi@hotmail.com>
2022-08-07 21:09:11 +0200

    Added factory provider interface

---
+++ Cadmus.Export/Preview/ICadmusPreviewFactoryProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace Cadmus.Export.Preview
+{
+    /// <summary>
+    /// Provider for <see cref="CadmusPreviewFactory"/>.
+    /// </summary>
+    public interface ICadmusPreviewFactoryProvider
+    {
+        /// <summary>
+        /// Gets the factory.
+        /// </summary>
+        /// <param name="profile">The profile.</param>
+        /// <returns>Factory.</returns>
+        CadmusPreviewFactory GetFactory(string profile);
+    }
+}
