ccfa2830f54c7d2c66d61b4a9b1060b511f861b0
Myrmex <dfusi@hotmail.com>
2022-08-14 21:57:07 +0200

    More composer

---
+++ Cadmus.Export.ML/FSTeiStandoffItemComposer.cs
@@ -0,0 +1,99 @@
+﻿using Cadmus.Core;
+using Fusi.Tools.Config;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Cadmus.Export.ML
+{
+    /// <summary>
+    /// File-based TEI standoff item composer.
+    /// <para>Tag: <c>it.vedph.item-composer.tei-standoff.fs</c>.</para>
+    /// </summary>
+    /// <seealso cref="ItemComposer" />
+    /// <seealso cref="IItemComposer" />
+    [Tag("it.vedph.item-composer.tei-standoff.fs")]
+    public sealed class FSTeiStandoffItemComposer : TeiStandoffItemComposer,
+        IItemComposer, IConfigurable<FSTeiStandoffItemComposerOptions>
+    {
+        private readonly Dictionary<string, TextWriter> _writers;
+        private FSTeiStandoffItemComposerOptions? _options;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FSTeiStandoffItemComposer"/>
+        /// class.
+        /// </summary>
+        public FSTeiStandoffItemComposer()
+        {
+            _writers = new Dictionary<string, TextWriter>();
+        }
+
+        /// <summary>
+        /// Configures the object with the specified options.
+        /// </summary>
+        /// <param name="options">The options.</param>
+        /// <exception cref="ArgumentNullException">options</exception>
+        public void Configure(FSTeiStandoffItemComposerOptions options)
+        {
+            _options = options ?? throw new ArgumentNullException(nameof(options));
+        }
+
+        /// <summary>
+        /// Open the composer.
+        /// </summary>
+        public override void Open()
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// Close the composer.
+        /// </summary>
+        public override void Close()
+        {
+            foreach (TextWriter writer in _writers.Values)
+            {
+                writer.Flush();
+                writer.Close();
+            }
+            _writers.Clear();
+        }
+
+        /// <summary>
+        /// Composes the output from the specified item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        /// <param name="context">The context.</param>
+        /// <returns>Composition result or null.</returns>
+        /// <exception cref="ArgumentNullException">item</exception>
+        public object? Compose(IItem item, object? context = null)
+        {
+            if (item is null) throw new ArgumentNullException(nameof(item));
+
+            Dictionary<string, string> flows = RenderFlows(item);
+            foreach (string key in flows.Keys)
+            {
+                if (!_writers.ContainsKey(key))
+                {
+                    _writers[key] = new StreamWriter(
+                        Path.Combine(_options!.OutputDirectory ?? "",
+                                     key + ".xml"), false, Encoding.UTF8);
+                }
+                _writers[key].WriteLine(flows[key]);
+            }
+            return flows;
+        }
+    }
+
+    /// <summary>
+    /// Options for <see cref="FSTeiStandoffItemComposer"/>.
+    /// </summary>
+    public class FSTeiStandoffItemComposerOptions
+    {
+        /// <summary>
+        /// Gets or sets the output directory.
+        /// </summary>
+        public string? OutputDirectory { get; set; }
+    }
+}
--- Cadmus.Export.ML/TeiStandoffItemComposer.cs
+++ Cadmus.Export.ML/TeiStandoffItemComposer.cs
@@ -1,15 +1,78 @@
 ﻿using Cadmus.Core;
 using Fusi.Tools.Config;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
 
 namespace Cadmus.Export.ML
 {
-    [Tag("it.vedph.item-composer.tei-standoff")]
-    public sealed class TeiStandoffItemComposer : ItemComposer, IItemComposer
+    public abstract class TeiStandoffItemComposer : ItemComposer
     {
-        public object? Compose(IItem item, object? context = null)
+        private readonly TextBlockBuilder _blockBuilder;
+        private readonly JsonSerializerOptions _jsonOptions;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TeiStandoffItemComposer"/>
+        /// class.
+        /// </summary>
+        protected TeiStandoffItemComposer()
         {
-            throw new NotImplementedException();
+            _blockBuilder = new();
+            _jsonOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+        }
+
+        private static string BuildLayerId(IPart part)
+        {
+            string id = part.TypeId;
+            if (!string.IsNullOrEmpty(part.RoleId)) id += "|" + part.RoleId;
+            return id;
+        }
+
+        protected Dictionary<string, string> RenderFlows(IItem item)
+        {
+            Dictionary<string, string> flows = new();
+
+            // text: there must be one
+            IPart? textPart = item.Parts.Find(
+                p => p.RoleId == PartBase.BASE_TEXT_ROLE_ID);
+
+            if (textPart == null || TextPartFlattener == null ||
+                TextBlockRenderer == null)
+            {
+                return flows;
+            }
+
+            // layers
+            IList<IPart> layerParts = item.Parts.Where(p =>
+                    p.RoleId.StartsWith(PartBase.FR_PREFIX)).ToList();
+
+            // flatten and render into blocks
+            var tr = TextPartFlattener.GetTextRanges(textPart,
+                layerParts,
+                (IList<string?>)layerParts.Select(p => BuildLayerId(p)).ToList());
+
+            List<TextBlockRow> rows =
+                _blockBuilder.Build(tr.Item1, tr.Item2).ToList();
+
+            // render blocks
+            flows[PartBase.BASE_TEXT_ROLE_ID] = TextBlockRenderer.Render(rows);
+
+            // render layers
+            foreach (IPart layerPart in layerParts)
+            {
+                string id = BuildLayerId(layerPart);
+                if (JsonRenderers.ContainsKey(id))
+                {
+                    string json = JsonSerializer.Serialize(layerPart, _jsonOptions);
+                    flows[id] = JsonRenderers[id].Render(json);
+                }
+            }
+
+            return flows;
         }
     }
 }
