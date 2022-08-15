2678478eddaaa15a88767b31e51e2a5705616343
Myrmex <dfusi@hotmail.com>
2022-08-15 09:28:56 +0200

    Adding ML tests

---
+++ Cadmus.Export.ML.Test/RamTeiStandoffItemComposer.cs
@@ -0,0 +1,22 @@
+﻿using Cadmus.Core;
+using Fusi.Tools.Config;
+
+namespace Cadmus.Export.ML.Test
+{
+    internal sealed class RamTeiStandoffItemComposer : TeiStandoffItemComposer,
+        IItemComposer, IConfigurable<TeiStandoffItemComposerOptions>
+    {
+        private TeiStandoffItemComposerOptions? _options;
+
+        public void Configure(TeiStandoffItemComposerOptions options)
+        {
+            _options = options
+                ?? throw new System.ArgumentNullException(nameof(options));
+        }
+
+        protected override object? DoCompose(IItem item, object? context = null)
+        {
+            return RenderFlows(item);
+        }
+    }
+}
