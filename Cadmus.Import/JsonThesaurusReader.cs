8f0c8c2b3c3defd1c8776e2a2e66301a2e91a585
Myrmex <dfusi@hotmail.com>
2023-06-18 10:38:15 +0200

    Adding import

---
+++ Cadmus.Import/JsonThesaurusReader.cs
@@ -0,0 +1,45 @@
+﻿using Cadmus.Core.Config;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadmus.Import;
+
+public sealed class JsonThesaurusReader : IThesaurusReader
+{
+    private bool _disposedValue;
+
+    public JsonThesaurusReader(string json)
+    {
+    }
+
+    public Thesaurus? Next()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Dispose(bool disposing)
+    {
+        if (!_disposedValue)
+        {
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects)
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            // TODO: set large fields to null
+            _disposedValue = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
