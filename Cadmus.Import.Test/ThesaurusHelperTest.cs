9d6d7535c2b778d8cc7aff4b7f9c1e9c32e7486a
Myrmex <dfusi@hotmail.com>
2023-06-19 22:22:35 +0200

    Update ThesaurusHelperTest.cs

--- Cadmus.Import.Test/ThesaurusHelperTest.cs
+++ Cadmus.Import.Test/ThesaurusHelperTest.cs
@@ -3,7 +3,7 @@ using Xunit;
 
 namespace Cadmus.Import.Test;
 
-public class ThesaurusHelperTest
+public sealed class ThesaurusHelperTest
 {
     private static Thesaurus GetRgbThesaurus()
     {
@@ -14,6 +14,7 @@ public class ThesaurusHelperTest
         return t;
     }
 
+    #region Replace
     [Fact]
     public void CopyThesaurus_Replace_NoAliasVsNoAlias_Replaced()
     {
@@ -40,7 +41,7 @@ public class ThesaurusHelperTest
     {
         Thesaurus source = new()
         {
-            TargetId = "target@en"
+            TargetId = "x@en"
         };
 
         Thesaurus target = GetRgbThesaurus();
@@ -50,7 +51,7 @@ public class ThesaurusHelperTest
 
         Assert.NotNull(result);
         Assert.Equal(0, result.Entries.Count);
-        Assert.Equal("target@en", result.TargetId);
+        Assert.Equal("x@en", result.TargetId);
     }
 
     [Fact]
@@ -60,7 +61,7 @@ public class ThesaurusHelperTest
 
         Thesaurus target = new()
         {
-            TargetId = "target@en"
+            TargetId = "x@en"
         };
 
         Thesaurus result = ThesaurusHelper.CopyThesaurus(source, target,
@@ -76,4 +77,69 @@ public class ThesaurusHelperTest
         Assert.Equal("blue", result.Entries[2].Value);
     }
 
+    [Fact]
+    public void CopyThesaurus_Replace_AliasVsAlias_Replaced()
+    {
+        Thesaurus source = new()
+        {
+            TargetId = "source@en"
+        };
+        Thesaurus target = new()
+        {
+            TargetId = "target@en"
+        };
+
+        Thesaurus result = ThesaurusHelper.CopyThesaurus(source, target,
+            ImportUpdateMode.Replace);
+
+        Assert.NotNull(result);
+        Assert.Empty(result.Entries);
+        Assert.Equal("source@en", result.TargetId);
+    }
+    #endregion
+
+    #region Patch
+    // all tests as above for replace, but using patch mode
+    [Fact]
+    public void CopyThesaurus_Patch_NoAliasVsNoAlias_Patched()
+    {
+        Thesaurus source = new();
+        source.AddEntry(new ThesaurusEntry { Id = "r", Value = "rosso" });
+        source.AddEntry(new ThesaurusEntry { Id = "g", Value = "verde" });
+
+        Thesaurus target = GetRgbThesaurus();
+
+        Thesaurus result = ThesaurusHelper.CopyThesaurus(source, target,
+                       ImportUpdateMode.Patch);
+
+        Assert.NotNull(result);
+        Assert.Equal(3, result.Entries.Count);
+        Assert.Equal("r", result.Entries[0].Id);
+        Assert.Equal("rosso", result.Entries[0].Value);
+
+        Assert.Equal("g", result.Entries[1].Id);
+        Assert.Equal("verde", result.Entries[1].Value);
+
+        Assert.Equal("b", result.Entries[2].Id);
+        Assert.Equal("blue", result.Entries[2].Value);
+    }
+
+    [Fact]
+    public void CopyThesaurus_Patch_AliasVsNoAlias_Alias()
+    {
+        Thesaurus source = new()
+        {
+            TargetId = "x@en"
+        };
+
+        Thesaurus target = GetRgbThesaurus();
+
+        Thesaurus result = ThesaurusHelper.CopyThesaurus(source, target,
+            ImportUpdateMode.Patch);
+
+        Assert.NotNull(result);
+        Assert.Equal(0, result.Entries.Count);
+        Assert.Equal("x@en", result.TargetId);
+    }
+    #endregion
 }
